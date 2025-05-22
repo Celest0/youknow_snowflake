b17c6a05473d91b9cc50bf0c1bc4ff8c0f25b645
doyley91 <baurpas@gmail.com>
2025-05-22 21:43:16 +0200

    app update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pysqlite3
+#import sqlite3
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+#sys.modules['sqlite3'] = __import__('pysqlite3')
 
 import pandas as pd
 import numpy as np
